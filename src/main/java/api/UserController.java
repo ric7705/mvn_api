9a801acbde255371b561115f57f035ef1c4392ca
ric <ric7705@gmail.com>
2019-10-20 14:56:36 +0800

    finish springboot skeleton

---
+++ src/main/java/api/UserController.java
@@ -0,0 +1,19 @@
+package api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class UserController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new String("hello");
+    }
+}
